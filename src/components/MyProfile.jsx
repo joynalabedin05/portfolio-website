1db8fbed8c81f2af0917a9b31bd58a1993c190fd
joynalabedin05 <joynal05101993@gmail.com>
2023-06-28 12:23:39 +0600

    added

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../public/FACEBOOK.jpg'
+import resume from '../../public/resume.jpg'
 const MyProfile = () => {
     return (
-        <div className='mt-20'> 
-            <h4>my profile</h4>
+        <div className='md:my-20 mt-40'>
+            <h3 className='text-4xl p-5 mb-8 rounded text text-center font-bold text-slate-200'> My Profile </h3>
+            <div className="card mx-auto md:w-1/2 bg-slate-700 shadow-xl">
+                <figure className="px-10 pt-10">
+                <div className=''>
+                    <Link className='md:mx-auto' to='/'><img className='w-32 h-24 md:ml-20 rounded-full' src={logo} alt="" /></Link>
+                    <h1 className='md:text-4xl text-lg  footer-title mb-0 mt-5'>JOYNAL ABEDIN</h1>
+                    <span className=''>Mern Stack Developer</span>
+                  
+                </div>
+                </figure>
+                <div className="card-body items-center text-center">
+                   <img className='rounded ' src={resume} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className=" dropdown-content mt-3 z-[1] p-2 shadow bg-slate-700 rounded-box w-60">
+                        <ul tabIndex={0} className=" dropdown-content mt-3 z-[1]  p-2 shadow bg-slate-700 rounded-box w-60">
                         <li className='hover:text-orange-300'><Link to='/'>Home</Link></li>                     
                         <li className='hover:text-orange-300'><Link to='/aboutme'>About Me</Link></li>
                         <li className='hover:text-orange-300'><Link to='/contacts'>Contacts</Link></li>                     
